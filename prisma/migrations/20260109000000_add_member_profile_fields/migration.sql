294a051de8fa64e89b5458fabaeda686ae814bc8
naroo-hosen <naroo.hosen@gmail.com>
2026-01-10 14:45:24 +0900

    Add migration for member profile fields

---
+++ prisma/migrations/20260109000000_add_member_profile_fields/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Member" ADD COLUMN "birthDate" DATETIME;
+ALTER TABLE "Member" ADD COLUMN "gender" TEXT;
+ALTER TABLE "Member" ADD COLUMN "parentPhone" TEXT;
+ALTER TABLE "Member" ADD COLUMN "memo" TEXT;
